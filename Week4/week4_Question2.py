60c20e51e1b3a526e3a138d7828edcfa34bbb0ed
MCurley <L00170156@student.lyit.ie>
2021-10-11 01:55:31 +0100

    Added Question 2 part 2.3

--- Week4/week4_Question2.py
+++ Week4/week4_Question2.py
@@ -15,38 +15,49 @@ import random
 if __name__ == '__main__':
 
     week_1 = ["Joe", "John", "Jane", "Mick", "Mary", "Ann", "Rick", "John", "Aine", "Brenda"]
-    wk1_list_size =len(week_1)
-    #Itnitialise the dictionary to hold the week 1 lotto numbers for each player
+    wk1_list_size = len(week_1)
+    # Itnitialise the dictionary to hold the week 1 lotto numbers for each player
     lotto_num_wk1 = {}
 
-    #Iterate over the list to generate lotto number
+    # Iterate over the list to generate lotto number
     for i in range(0, wk1_list_size):
-        num = random.randint(1,20)
+        num = random.randint(1, 20)
         lotto_num_wk1[week_1[i]] = num
 
-    #print(lotto_num_wk1)
+    # print(lotto_num_wk1)
 
     week_2 = ["Jack", "Mary", "Phil", "John", "Pat", "Joe", "Luke", "Bill", "Ben", "Nathan"]
     wk2_list_size = len(week_2)
     lotto_num_wk2 = {}
 
     for i in range(0, wk2_list_size):
-        num = random.randint(1,20)
+        num = random.randint(1, 20)
         lotto_num_wk2[week_2[i]] = num
 
-    #print(lotto_num_wk2)
-    
+    # print(lotto_num_wk2)
+
     played_both_weeks = []
     unique_names = []
     week_1_set = set(week_1)
     week_2_set = set(week_2)
 
-    #Question 2.1 - Who bought tickets on both weeks
+    # Question 2.1 - Who bought tickets on both weeks
     played_both_weeks = week_1_set & week_2_set
     print(played_both_weeks)
 
-    #Question 2.2 - Unique players over both weeks
+    # Question 2.2 - Unique players over both weeks
     unique_names = week_1_set | week_2_set
     print(unique_names)
 
-    #Question 2.3 - Most common lotto numbers
+    # Question 2.3 - Most common lotto numbers
+
+    all_lotto_numbers = list(lotto_num_wk1.values()) + list(lotto_num_wk2.values())
+    print(all_lotto_numbers)
+
+    from collections import Counter
+
+    c = Counter(all_lotto_numbers)
+
+    print(c)
+
+    # Question 2.4 - Did any member pick the same number on multiple occasions
